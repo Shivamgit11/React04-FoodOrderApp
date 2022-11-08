0a3c01010ae3764122f8766dca04fdeadd9e0b3c
shivam <mailtoshivam2002@gmail.com>
2022-11-08 22:22:48 +0530

    final submission after resolving all error

--- src/store/Cartprovider.js
+++ src/store/Cartprovider.js
@@ -5,14 +5,25 @@ const CartProvider = (props) => {
   const [items, updateItems] = useState([]);
 
   const addItemToCartHandler = (item, quantity) => {
-    //console.log(item,quantity)
-    //item.quantity = 1;
-    updateItems([...items, { ...item, quantity: parseInt(quantity) || 0 }]);
-    //console.log('inside addItemTocartHandler', cartContext)
+    const itemToUpdate = items.find((cartItem) => cartItem.id === item.id);
+
+    if (itemToUpdate) {
+      const updatedItemObj = {
+        ...itemToUpdate,
+        quantity: itemToUpdate.quantity + 1
+      };
+
+      const newItemArray = items.map((cartItem) =>
+        cartItem.id === updatedItemObj.id ? updatedItemObj : cartItem
+      );
+
+      updateItems(newItemArray);
+    } else {
+      updateItems([...items, { ...item, quantity: parseInt(quantity) || 0 }]);
+    }
   };
 
   const increaseCartItemQty = (id) => {
-    console.log("increaseCartItemQty", id);
     const cartItems = [...items];
     const itemIdx = cartItems.findIndex((item) => item.id === id);
     cartItems[itemIdx].quantity += 1;
@@ -20,11 +31,19 @@ const CartProvider = (props) => {
   };
 
   const DecreaseCartQty = (id) => {
-    console.log("increaseCartItemQty", id);
     const cartItems2 = [...items];
     const itemIdx = cartItems2.findIndex((item) => item.id === id);
-    cartItems2[itemIdx].quantity -= 1;
-    updateItems(cartItems2);
+
+    if (cartItems2[itemIdx].quantity === 1) {
+      const newItemArray = cartItems2.filter(
+        (cartItem) => cartItem.id !== cartItems2[itemIdx].id
+      );
+
+      updateItems(newItemArray);
+    } else {
+      cartItems2[itemIdx].quantity -= 1;
+      updateItems(cartItems2);
+    }
   };
 
   const removeItemFromCartHandler = (id) => {};
@@ -40,7 +59,6 @@ const CartProvider = (props) => {
 
   return (
     <CartContext.Provider value={cartContext}>
-      {/* {console.log('Inside Cartcontext.Provider', cartContext)} */}
       {props.children}
     </CartContext.Provider>
   );
